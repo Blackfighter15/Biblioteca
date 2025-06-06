fcb7cab251d53627b7421544083d401ca5b13f29
Blackfighter15 <karass.cota.cesar@gmail.com>
2025-06-06 15:14:37 -0700

    b

--- Biblioteca-django/apps/biblioteca/urls.py
+++ Biblioteca-django/apps/biblioteca/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
+      path('login/', LoginView.as_view(template_name='biblioteca/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
     path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
     path('usuarios/nuevo/', views.nuevo_usuario, name='nuevo_usuario'),
